111c58bec00cd1fd34ac6f8b5ac22ff31871d092
Cesar Lima <macpro@MacBook-Pro-de-MacPro.local>
2019-07-22 18:36:10 -0300

    productOfArray challenge fixed

--- recursion/product-of-array/index.js
+++ recursion/product-of-array/index.js
@@ -4,9 +4,11 @@
  */
 
  function productOfArray(array) {
-     if (array.length === 0) return 0
-     return array[0] + productOfArray(array.slice(1))
+     if (array.length === 0) return 1
+     return array[0] * productOfArray(array.slice(1))
  }
 
+ console.log(productOfArray([1, 2, 3]))
+ console.log(productOfArray([1, 2, 3, 10]))
  console.log(productOfArray([1, 2, 3, 4, 5]))
  console.log(productOfArray([10, 28, 73, 41, 50]))
