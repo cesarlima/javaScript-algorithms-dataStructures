dbd1c05cffeabf80a1657f5f503b8833517a8c6c
Cesar <cesarcamposdelima@gmail.com>
2019-10-26 13:13:09 -0300

    Add comment in are-there-duplicates challenge

--- algorithms/multiple-pointers/are-there-duplicates/index.js
+++ algorithms/multiple-pointers/are-there-duplicates/index.js
@@ -1,3 +1,12 @@
+/**
+ *  Implement a function called, areThereDuplicates which accepts a variable number of arguments
+ *  and checks whether there are any duplicates among the arguments passed in.
+ * 
+ *  Restrictions:
+ *  Time - O(n)
+ *  Space - O(n)
+ */
+
 (function() {
     'use strict'
 
